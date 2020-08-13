b8d0b67d48886221ef321bd8c659be8e3aea6d27
zhangxiaoxuan9 <1438208831@qq.com>
2020-08-13 20:54:58 +0800

    初始化首页和备忘录

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,7 +1,23 @@
 const app = getApp()
 Page({
   data: {
-   
+    indexlist: [
+      {
+        imagePath: "/images/lunbotu/one.jpg",
+      },
+      {
+        imagePath: "/images/lunbotu/two.jpg",
+      },
+      {
+        imagePath: "/images/lunbotu/three.jpg",
+      },
+      {
+        imagePath: "/images/lunbotu/four.jpg",
+      },
+      {
+        imagePath: "/images/lunbotu/five.jpg",
+      }
+    ]
   },
   onLoad: function() {
 
---
+++ miniprogram/pages/me/me.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    melist: [
+      {
+        imagePath: "/images/lunbotu/one.jpg",
+      },
+      {
+        name: "/images/geren/four.jpg",
+        imagePath: "/images/geren/five.jpg",
+        time: "/images/geren/six.jpg",
+      },
+
+    ]
+  }
+})

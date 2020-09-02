7e981a27cb50603f7c03ef33be9355679ac38de9
MYvLiang <13030235667@163.com>
2020-09-02 15:05:28 +0800

    改名为iknow佛大

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -378,7 +378,9 @@ Page({
   },
   onShareAppMessage: function () {
     return {
-      path: '/pages/index/index'
+      title: '邀你使用iknow佛大',
+      path: '/pages/index/index',
+      imageUrl: 'https://fosuwxapp.gitee.io/wxapp-images/indexImages/shareapp200902.png'
     }
   }
 })
